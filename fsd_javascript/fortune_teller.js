4834326371dd6fc44628e540e4210338f300c858
anushkayadav0024-dev <anushkayadav0024@gmail.com>
2026-07-02 09:33:37 +0530

    Amazon Clone Project using HTML & CSS

---
+++ fsd_javascript/fortune_teller.js
@@ -0,0 +1,5 @@
+let child=2;
+let partner="Anushka";
+let location="Delhi";
+let job="Software Developer";
+console.log(`You will be a ${job} in ${location}, married to ${partner} with ${child} kids`);
